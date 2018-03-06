24284ca8ab3a378ced1515f3009245f8110e679a
Samuli <jaaskelainen.samuli@gmail.com>
2018-03-06 21:49:02 +0200

    Heavily commented level manager.

--- LevelManager.gd
+++ LevelManager.gd
@@ -1,19 +1,35 @@
 extends Node
 
+# Pieces currently in level
 var currentLevel = []
+
+# Pieces used in random pool
 var levelPieces = []
+
+# Add pieces here, piece1, piece2, piece3 etc.
+# Pieces should be in LevelPieces folder
+# Each piece var needs to point to different level piece
+# MIGI ADD NEW PIECES HERE
 var piece1 = preload("res://LevelPieces/LevelPiece1.tscn")
 
 func _ready():
+	# Here add pieces that you want to use to levelPieces list
+	# Comment out with #-sign pieces you don't want in your random pool
+	# MIGI COMMENT OUT PIECES YOU DON'T WANT TO TEST
 	levelPieces.append(piece1)
+	
+	# Once all pieces are added we generate initial level
 	generateLevel()
 
 func generateLevel():
+	# When game is booted, we need to add beginning piece
 	if currentLevel.size() == 0:
 		createPiece(Vector2(256,128))
 	
+	# Piece sizes are hardcoded
 	createPiece(Vector2(768,128))
 
+# Pick a piece and spawn it, this is called when current piece goes out of screen
 func createPiece(var pos):
 	var level = levelPieces[randi()%levelPieces.size()].instance()
 	level.position = pos
