4498d9eb8e15b30a679b972f6ce4cf6c3c61ab5a
Samuli <jaaskelainen.samuli@gmail.com>
2018-03-10 14:56:17 +0200

    Rail shot has now actual width.

--- Guns/RailShot.gd
+++ Guns/RailShot.gd
@@ -1,6 +1,5 @@
 extends "res://Guns/SingleShot.gd"
 
-var offset = Vector2(0,-4)
 var shot = false
 
 func startMove():
@@ -10,17 +9,39 @@ func startMove():
 func move(val):
 	
 	if val > 0.9 and not shot:
+		var hitEnemies = []
 		shot = true
-		$RayCast2D.force_raycast_update()
 		var rayLenght = 512
 		var playerPos = get_parent().find_node("Player").global_position
-		$RayCast2D.global_position = playerPos + offset
-		$Sprite.global_position = playerPos + offset
+		$RayCast2D.global_position = playerPos + Vector2(0, -4)
+		$RayCast2DUp.global_position = playerPos + Vector2(0, 7)
+		$RayCast2DDown.global_position = playerPos + Vector2(0,-1)
+		$Sprite.global_position = playerPos + Vector2(0, -4)
+
+		$RayCast2D.force_raycast_update()
 		if $RayCast2D.is_colliding():
 			if "Enemy" in $RayCast2D.get_collider().get_name():
+				hitEnemies.append($RayCast2D.get_collider().get_name())
 				$RayCast2D.get_collider().hit(damage)
+				print("Railing0 %s", $RayCast2D.get_collider().get_name())
 			rayLenght = $RayCast2D.get_collision_point().x - playerPos.x
-			#print("Railing %s", $RayCast2D.get_collider().get_name())
+				
+		$RayCast2DUp.cast_to = Vector2(rayLenght, 0)
+		$RayCast2DUp.force_raycast_update()
+		if $RayCast2DUp.is_colliding():
+			if "Enemy" in $RayCast2DUp.get_collider().get_name() and not hitEnemies.has($RayCast2DUp.get_collider().get_name()):
+				hitEnemies.append($RayCast2DUp.get_collider().get_name())
+				$RayCast2DUp.get_collider().hit(damage)
+				print("Railing1 %s", $RayCast2D.get_collider().get_name())
+				
+		$RayCast2DDown.cast_to = Vector2(rayLenght, 0)
+		$RayCast2DDown.force_raycast_update()
+		if $RayCast2DDown.is_colliding():
+			if "Enemy" in $RayCast2DDown.get_collider().get_name() and not hitEnemies.has($RayCast2DDown.get_collider().get_name()):
+				hitEnemies.append($RayCast2DDown.get_collider().get_name())
+				$RayCast2DDown.get_collider().hit(damage)
+				print("Railing2 %s", $RayCast2D.get_collider().get_name())
+		
 		$Sprite.scale.x = rayLenght
 	
 func endMove():
